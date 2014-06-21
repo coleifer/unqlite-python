d6ed7a2a54afe765117fa3bbb0aa404ef8060774
Charles Leifer <coleifer@gmail.com>
2014-06-21 13:25:14 -0500

    Adding more, including set w/format and fetch w/callback.

--- unqlite/core.py
+++ unqlite/core.py
@@ -1,9 +1,13 @@
+from functools import wraps
+
 from unqlite._unqlite import *
 from unqlite._unqlite import _libs as _c_libraries
+from unqlite._unqlite import _variadic_function
 
 def handle_return_value(rc):
     if rc != UNQLITE_OK:
         raise Exception({
+            UNQLITE_NOTFOUND: 'Key not found',
             UNQLITE_NOMEM: 'Out of memory',
             UNQLITE_ABORT: 'Executed command request an operation abort',
             UNQLITE_IOERR: 'OS error',
@@ -36,14 +40,35 @@ class UnQLite(object):
     def close(self):
         handle_return_value(unqlite_close(self._unqlite))
 
+    def config(self, verb, *args):
+        return handle_return_value(_unqlite_lib.unqlite_config(
+            self._unqlite,
+            verb,
+            *args))
+
+    def _disable_autocommit(self):
+        self.config(UNQLITE_CONFIG_DISABE_AUTO_COMMIT)
+
+    def _set_max_page_cache(self, npages):
+        self.config(UNQLITE_CONFIG_MAX_PAGE_CACHE, npages)
+
+    # Key/Value interface.
     def store(self, key, value):
         key, value = str(key), str(value)
         handle_return_value(unqlite_kv_store(
             self._unqlite,
             key,
-            len(key),
+            -1,
             value,
             len(value)))
+
+    def store_fmt(self, key, value, *params):
+        return handle_return_value(_unqlite_lib.unqlite_kv_store_fmt(
+            self._unqlite,
+            str(key),
+            -1,
+            value,
+            *params))
 
     def append(self, key, value):
         key, value = str(key), str(value)
@@ -71,7 +96,7 @@ class UnQLite(object):
         rc = unqlite_kv_fetch(
             self._unqlite,
             key,
-            len(key),
+            -1,
             byref(buf),
             byref(nbytes))
         if rc == UNQLITE_OK:
@@ -79,6 +104,39 @@ class UnQLite(object):
         elif rc == SXERR_NOTFOUND:
             raise KeyError(key)
         handle_return_value(rc)
+
+    def fetch_cb(self, key, callback):
+        if not hasattr(callback, '_c_callback'):
+            callback = self.kv_callback(callback)
+        rc = unqlite_kv_fetch_callback(
+            self._unqlite,
+            key,
+            -1,
+            callback._c_callback,
+            None)
+        if rc == UNQLITE_NOTFOUND:
+            raise KeyError(key)
+        return handle_return_value(rc)
+
+    def kv_callback(self, fn):
+        def cb_wrapper(data_ptr, data_len, user_data_ptr):
+            data = (c_char * data_len).from_address(data_ptr).raw
+            try:
+                fn(data)
+            except:
+                return UNQLITE_ABORT
+            return UNQLITE_OK
+
+        c_callback = CFUNCTYPE(
+            UNCHECKED(c_int),
+            POINTER(None),
+            UNCHECKED(c_uint),
+            POINTER(None))(cb_wrapper)
+
+        fn._c_callback = c_callback
+        fn._cb_wrapper = cb_wrapper
+
+        return fn
 
     def delete(self, key):
         key = str(key)
@@ -88,3 +146,44 @@ class UnQLite(object):
     __setitem__ = store
     __delitem__ = delete
     __contains__ = exists
+
+    # Transaction helpers. Currently these do not seem to work, not sure why.
+    def begin(self):
+        handle_return_value(unqlite_begin(self._unqlite))
+
+    def commit(self):
+        handle_return_value(unqlite_commit(self._unqlite))
+
+    def rollback(self):
+        handle_return_value(unqlite_rollback(self._unqlite))
+
+    def transaction(self):
+        return transaction(self)
+
+    def commit_on_success(self, fn):
+        @wraps(fn)
+        def wrapper(*args, **kwargs):
+            with self.transaction():
+                return fn(*args, **kwargs)
+        return wrapper
+
+    # Cursor helpers.
+
+
+class transaction(object):
+    def __init__(self, unqlite):
+        self.unqlite = unqlite
+
+    def __enter__(self):
+        self.unqlite.begin()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type:
+            self.unqlite.rollback()
+        else:
+            try:
+                self.unqlite.commit()
+            except:
+                self.unqlite.rollback()
+                raise
--- unqlite/tests.py
+++ unqlite/tests.py
@@ -9,6 +9,8 @@ except ImportError:
     sys.stderr.flush()
     raise
 
+from _unqlite import UNQLITE_OK
+
 
 class BaseTestCase(unittest.TestCase):
     def setUp(self):
@@ -50,6 +52,72 @@ class TestKeyValueStorage(BaseTestCase):
 
         self.db.append('k2', 'V2')
         self.assertEqual(self.db['k2'], 'V2')
+
+    def test_store_fmt(self):
+        self.db.store_fmt('k1', 'foo %s:%d:%s', 'v1', 25, 'VX')
+        self.assertEqual(self.db['k1'], 'foo v1:25:VX')
+
+    def test_fetch_cb(self):
+        state = []
+
+        @self.db.kv_callback
+        def cb(data):
+            state.append(data)
+
+        @self.db.kv_callback
+        def alt_cb(data):
+            state.append(data.upper())
+
+        self.db['k1'] = 'v1'
+        self.db['k2'] = 'v2'
+
+        self.db.fetch_cb('k1', cb)
+        self.assertEqual(state, ['v1'])
+
+        self.db.fetch_cb('k2', cb)
+        self.assertEqual(state, ['v1', 'v2'])
+
+        self.db.fetch_cb('k1', alt_cb)
+        self.assertEqual(state, ['v1', 'v2', 'V1'])
+
+        self.assertRaises(KeyError, lambda: self.db.fetch_cb('kx', cb))
+
+class TestTransaction(BaseTestCase):
+    """
+    These tests do not behave as I expect, but I am documenting the behavior
+    here just in the event it changes.
+    """
+    def test_transaction(self):
+        @self.db.commit_on_success
+        def _test_success(key, value):
+            self.db[key] = value
+
+        @self.db.commit_on_success
+        def _test_failure(key, value):
+            self.db[key] = value
+            raise Exception('intentional exception raised')
+
+        _test_success('k1', 'v1')
+        self.assertEqual(self.db['k1'], 'v1')
+
+        self.assertRaises(Exception , lambda: _test_failure('k2', 'v2'))
+        # I am not sure why this is the case, but the transaction management
+        # does not seem to work.
+        # self.assertRaises(KeyError, lambda: self.db['k2'])
+        self.assertEqual(self.db['k2'], 'v2')
+
+    def test_explicit_transaction(self):
+        self.db.close()
+        self.db.open()
+        self.db.begin()
+        self.db['k1'] = 'v1'
+        self.db.rollback()
+
+        # Again, not sure why this does not work, but for some reason, despite
+        # the rollback, the value is still present.
+        # self.assertRaises(KeyError, lambda: self.db['k1'])
+        self.assertEqual(self.db['k1'], 'v1')
+
 
 if __name__ == '__main__':
     unittest.main(argv=sys.argv)
