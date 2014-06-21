d64c2ece57582668e246d9d211f4b423f997856d
Charles Leifer <coleifer@gmail.com>
2014-06-21 11:50:19 -0500

    Starting work with the KV interface.

--- unqlite/core.py
+++ unqlite/core.py
@@ -1,2 +1,90 @@
 from unqlite._unqlite import *
 from unqlite._unqlite import _libs as _c_libraries
+
+def handle_return_value(rc):
+    if rc != UNQLITE_OK:
+        raise Exception({
+            UNQLITE_NOMEM: 'Out of memory',
+            UNQLITE_ABORT: 'Executed command request an operation abort',
+            UNQLITE_IOERR: 'OS error',
+            UNQLITE_CORRUPT: 'Corrupt database',
+            UNQLITE_LOCKED: 'Locked database',
+            UNQLITE_BUSY: 'Database is locked by another thread/process',
+            UNQLITE_DONE: 'Error: UNQLITE_DONE',
+            UNQLITE_NOTIMPLEMENTED: 'Not implemented',
+            UNQLITE_READ_ONLY: 'Database is in read-only mode',
+        }.get(rc, 'Unknown exception: %s' % rc))
+
+_unqlite_lib = _c_libraries['unqlite']
+
+class UnQLite(object):
+    """
+    UnQLite database python bindings.
+    """
+    def __init__(self, database=':mem:', open_manually=False):
+        self.database = database
+        self._unqlite = POINTER(unqlite)()
+        self._command_registry = {}
+        if not open_manually:
+            self.open()
+
+    def open(self, flags=UNQLITE_OPEN_CREATE):
+        rc = unqlite_open(byref(self._unqlite), self.database, flags)
+        if rc != UNQLITE_OK:
+            raise Exception('Unable to open UnQLite database')
+
+    def close(self):
+        handle_return_value(unqlite_close(self._unqlite))
+
+    def store(self, key, value):
+        key, value = str(key), str(value)
+        handle_return_value(unqlite_kv_store(
+            self._unqlite,
+            key,
+            len(key),
+            value,
+            len(value)))
+
+    def append(self, key, value):
+        key, value = str(key), str(value)
+        handle_return_value(unqlite_kv_append(
+            self._unqlite,
+            key,
+            len(key),
+            value,
+            len(value)))
+
+    def exists(self, key):
+        length = unqlite_int64(0)
+        key = str(key)
+        return unqlite_kv_fetch(
+            self._unqlite,
+            key,
+            len(key),
+            None,
+            byref(length)) == UNQLITE_OK
+
+    def fetch(self, key, buf_size=4096, determine_buffer_size=False):
+        key = str(key)
+        buf = create_string_buffer(buf_size)
+        nbytes = unqlite_int64(buf_size)
+        rc = unqlite_kv_fetch(
+            self._unqlite,
+            key,
+            len(key),
+            byref(buf),
+            byref(nbytes))
+        if rc == UNQLITE_OK:
+            return buf.raw[:nbytes.value]
+        elif rc == SXERR_NOTFOUND:
+            raise KeyError(key)
+        handle_return_value(rc)
+
+    def delete(self, key):
+        key = str(key)
+        handle_return_value(unqlite_kv_delete(self._unqlite, key, len(key)))
+
+    __getitem__ = fetch
+    __setitem__ = store
+    __delitem__ = delete
+    __contains__ = exists
--- unqlite/tests.py
+++ unqlite/tests.py
@@ -11,8 +11,45 @@ except ImportError:
 
 
 class BaseTestCase(unittest.TestCase):
-    pass
+    def setUp(self):
+        super(BaseTestCase, self).setUp()
+        self.db = UnQLite(':mem:')
 
+
+class TestKeyValueStorage(BaseTestCase):
+    def test_basic_operations(self):
+        self.db.store('k1', 'v1')
+        self.db.store('k2', 'v2')
+        self.assertEqual(self.db.fetch('k1'), 'v1')
+        self.assertEqual(self.db.fetch('k2'), 'v2')
+        self.assertRaises(KeyError, self.db.fetch, 'k3')
+
+        self.db.delete('k2')
+        self.assertRaises(KeyError, self.db.fetch, 'k2')
+
+        self.assertTrue(self.db.exists('k1'))
+        self.assertFalse(self.db.exists('k2'))
+
+    def test_dict_interface(self):
+        self.db['k1'] = 'v1'
+        self.db['k2'] = 'v2'
+        self.assertEqual(self.db['k1'], 'v1')
+        self.assertEqual(self.db['k2'], 'v2')
+        self.assertRaises(KeyError, lambda: self.db['k3'])
+
+        del self.db['k2']
+        self.assertRaises(KeyError, lambda: self.db['k2'])
+
+        self.assertTrue('k1' in self.db)
+        self.assertFalse('k2' in self.db)
+
+    def test_append(self):
+        self.db['k1'] = 'v1'
+        self.db.append('k1', 'V1')
+        self.assertEqual(self.db['k1'], 'v1V1')
+
+        self.db.append('k2', 'V2')
+        self.assertEqual(self.db['k2'], 'V2')
 
 if __name__ == '__main__':
     unittest.main(argv=sys.argv)
