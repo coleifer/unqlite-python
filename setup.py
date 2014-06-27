5f569da6048b4ce92986243a03d802ebd6c9268f
Charles Leifer <coleifer@gmail.com>
2014-06-26 21:04:48 -0500

    Bump

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ class GenerateCtypesWrapper(build_py):
 
 setup(
     name='unqlite',
-    version='0.1.1',
+    version='0.1.2',
     description='Python bindings for UnQLite, the embedded NoSQL database.',
     author='Charles Leifer',
     author_email='',
