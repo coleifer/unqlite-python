945d11713ecf964eb1b031cdcb823b5e078e2e6e
Charles Leifer <coleifer@gmail.com>
2015-07-11 00:19:34 -0500

    Cleanup.

--- setup.py
+++ setup.py
@@ -1,30 +1,24 @@
 import os
 
 from distutils.core import setup, Extension
-from Cython.Build import cythonize
+try:
+    from Cython.Build import cythonize
+except ImportError:
+    raise RuntimeError('Cython must be installed in order to build '
+                       'unqlite-python.')
 
 
-cur_dir = os.path.dirname(__file__)
-cython_module = os.path.join(cur_dir, 'unqlite/_unqlite.pyx')
-c_source = os.path.join(cur_dir, 'unqlite/src/unqlite.c')
+cython_module = 'unqlite.pyx'
+c_source = 'src/unqlite.c'
 unqlite_extension = Extension(
-    os.path.join(cur_dir, 'unqlite/_unqlite'),
+    'unqlite',
     sources=[cython_module, c_source])
 
 setup(
     name='unqlite',
-    version='0.3.0',
+    version='0.3.2',
     description='Fast Python bindings for the UnQLite embedded NoSQL database.',
     author='Charles Leifer',
     author_email='',
-    packages=['unqlite'],
     ext_modules=cythonize(unqlite_extension),
-    install_requires=['Cython'],
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-    ],
 )
