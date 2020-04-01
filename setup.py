57b156118015be011246bb6cfa112e1ab9368b49
Charles Leifer <coleifer@gmail.com>
2020-04-01 12:47:37 -0500

    0.8.1

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ unqlite_extension = Extension(
 
 setup(
     name='unqlite',
-    version='0.8.0',
+    version='0.8.1',
     description='Fast Python bindings for the UnQLite embedded NoSQL database.',
     author='Charles Leifer',
     author_email='',
