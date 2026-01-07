0a45eb9534da2b4eb4b6cb7439879f948d46b420
Charles Leifer <coleifer@gmail.com>
2026-01-06 19:35:05 -0600

    1.0.0
    New build system

--- setup.py
+++ setup.py
@@ -1,17 +1,11 @@
-import glob
 import sys
-import warnings
-
 from setuptools import setup
 from setuptools.extension import Extension
 try:
     from Cython.Build import cythonize
+    cython_installed = True
 except ImportError:
     cython_installed = False
-    warnings.warn('Cython not installed, using pre-generated C source file.')
-else:
-    cython_installed = True
-
 
 if cython_installed:
     python_source = 'unqlite.pyx'
@@ -31,40 +25,4 @@ unqlite_extension = Extension(
     libraries=libs,
     sources=[python_source] + library_source)
 
-setup(
-    name='unqlite',
-    version='0.9.9',
-    description='Fast Python bindings for the UnQLite embedded NoSQL database.',
-    author='Charles Leifer',
-    author_email='',
-    url='https://github.com/coleifer/unqlite-python',
-    license='MIT',
-    #install_requires=['Cython'],
-    #setup_requires=['cython'],
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: C',
-        'Programming Language :: Cython',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
-        'Programming Language :: Python :: 3.13',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Topic :: Database',
-        'Topic :: Database :: Database Engines/Servers',
-        'Topic :: Software Development :: Embedded Systems',
-        'Topic :: Software Development :: Libraries :: Python Modules'],
-    ext_modules=cythonize([unqlite_extension])
-)
+setup(name='unqlite', ext_modules=cythonize([unqlite_extension]))
